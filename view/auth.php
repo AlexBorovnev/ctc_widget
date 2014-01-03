99b4b551d9bef8e08b3e80006d32446caa4e2ff0
Alexandr Borovnev <andersen.borovnev@gmail.com>
2014-01-03 18:31:59 +0300

    add style for login page

--- view/auth.php
+++ view/auth.php
@@ -1,17 +1,65 @@
 <?php
 use library\Common;
-if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['login']) && !empty($_POST['password'])):
-    if($id = Common::getInstance()->getUserId($_POST['login'], $_POST['password'])):
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['login']) && !empty($_POST['password'])):
+    if ($id = Common::getInstance()->getUserId($_POST['login'], $_POST['password'])):
         $_SESSION['user_id'] = $id;
     endif;
 endif;
 ?>
-<?php if (empty($_SESSION['user_id'])):?>
-<form name="auth_form" action="<?=HOST?>admin" method="post">
-    <label for="login">Login:</label>
-    <input type="text" name="login" id="login" placeholder="Your login"/>
-    <label for="password">Password:</label>
-    <input type="password" name="password" id="password" placeholder="Your password"/>
-    <input type="submit" name="btn_send" value="Login" />
-</form>
-<?php exit; endif;?>
+<?php if (empty($_SESSION['user_id'])): ?>
+    <head>
+    <title>Sign In</title>
+    <script type="text/javascript" src="<?= HOST ?>js/admin/jquery-1.9.1.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?= HOST ?>css/bootstrap.min.css?<?= REV ?>">
+        <style type="text/css">
+            body {
+                padding-top: 40px;
+                padding-bottom: 40px;
+                background-color: #f5f5f5;
+            }
+
+            .form-signin {
+                max-width: 300px;
+                padding: 19px 29px 29px;
+                margin: 0 auto 20px;
+                background-color: #fff;
+                border: 1px solid #e5e5e5;
+                -webkit-border-radius: 5px;
+                -moz-border-radius: 5px;
+                border-radius: 5px;
+                -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                background: url(images/sts_logo.png) no-repeat center;
+                -moz-background-size: 100% auto;
+                -webkit-background-size: 100% auto;
+                -o-background-size: 100% auto;
+                background-size: 100% auto;
+
+            }
+            .form-signin .form-signin-heading,
+            .form-signin .checkbox {
+                margin-bottom: 10px;
+            }
+            .form-signin input[type="text"],
+            .form-signin input[type="password"] {
+                font-size: 16px;
+                height: auto;
+                margin-bottom: 15px;
+                padding: 7px 9px;
+            }
+
+        </style>
+    </head>
+    <div class="container">
+        <form class="form-signin" name="auth_form" action="<?= HOST ?>admin" method="post">
+            <h3 class="form-signin-heading">Please sign in</h3>
+            <label for="login">Login:</label>
+            <input type="text" class="input-block-level" name="login" id="login" placeholder="Your login"/>
+            <label for="password">Password:</label>
+            <input class="input-block-level" type="password" name="password" id="password" placeholder="Your password"/>
+            <input type="submit" class="btn btn-large btn-primary" name="btn_send" value="Login"/>
+        </form>
+    </div>
+    <?php exit; endif; ?>
