bbfb9ffcd3c0bd66ad694f886a05d0335d1976b9
Alexandr Borovnev <andersen.borovnev@gmail.com>
2014-02-18 14:40:53 +0300

    modified 404 error page

--- view/404.php
+++ view/404.php
@@ -1,3 +1,6 @@
 <?php
-header("Location: ".HOST."404.html");
+ob_end_clean();
+header('HTTP/1.0 404 Not Found');
+//echo "<h1>Error 404 Not Found</h1>";
+//echo "The page that you have requested could not be found.";
 exit;
